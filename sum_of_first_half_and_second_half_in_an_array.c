a8f225063215f11d053dce3849537143b367cf85
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-06-28 22:12:51 +0530

    #include<stdio.h> int main() {     int n,a[100],i,sum1=0,sum2=0,p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n/2;i++)     {         sum1+=a[i];     }     for(i=n/2;i<n;i++)     {         sum2+=a[i];     }     printf("%d ",sum1);     printf("%d",sum2); }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,sum1=0,sum2=0,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        sum1+=a[i];
+    }
+    for(i=n/2;i<n;i++)
+    {
+        sum2+=a[i];
+    }
+    printf("%d
+",sum1);
+    printf("%d",sum2);
+}
