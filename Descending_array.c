2443ccefd03e6c395fda6d93672ccf63345d6d46
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-06-28 21:30:43 +0530

    #include<stdio.h> int main() {     int n,a[100],r,i,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]>a[i+1])         {             flag++;         }     }     if(flag==n)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Descending_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],r,i,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            flag++;
+        }
+    }
+    if(flag==n)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
