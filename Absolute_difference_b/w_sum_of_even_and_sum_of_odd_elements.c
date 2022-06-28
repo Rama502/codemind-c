3df1758776d7cefb70c75f046c827432319a00f5
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-06-28 23:11:55 +0530

    #include<stdio.h> int main() {     int n,a[100],i,sum1=0,sum2=0,p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             sum1+=a[i];         }     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             sum2+=a[i];         }     }     p=sum2-sum1;     if(p>0)     {         printf("%d",p);     }     else     {         printf("%d",-p);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,sum1=0,sum2=0,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            sum1+=a[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            sum2+=a[i];
+        }
+    }
+    p=sum2-sum1;
+    if(p>0)
+    {
+        printf("%d",p);
+    }
+    else
+    {
+        printf("%d",-p);
+    }
+}
