337b0c7d14d91621288d5a6c34971eb16e05d9d1
Rama502 <97427355+Rama502@users.noreply.github.com>
2022-07-18 21:35:25 +0530

    #include<stdio.h> int main() {     int n,i,fact=0,j,count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             fact=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     fact=fact+1;                 }             }             if(fact!=2)             {                 count+=1;             }         }     }     printf("%d",count);     return 0; }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int N,i=1,count=0;
-	scanf("%d",&N);
-	for(i;i<=N;i++)
-	{
-		int sum=0;
-		if(N%i==0)
-		{
-			int j=1;
-			while(j<=i)
-			{
-				if(i%j==0)
-				{
-					sum++;
-				}
-				j++;
-			}
-			if(sum!=2)
-			{
-				count++;
-			}
-		}
-	}
-	printf("%d",count);
+    int n,i,fact=0,j,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            fact=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    fact=fact+1;
+                }
+            }
+            if(fact!=2)
+            {
+                count+=1;
+            }
+        }
+    }
+    printf("%d",count);
+    return 0;
 }
